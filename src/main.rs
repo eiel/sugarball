18988b03ad0cbba4911b0c1e6658c8c1b7c2788a
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-22 20:55:51 +0900

    parse json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
 use std::process::Command;
 use std::string::String;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Formula {
+    full_name: String,
+    linked_keg: Option<String>,
+}
 
 fn main() {
     let c = installed_list();
@@ -15,10 +27,10 @@ fn get_installed_info_command() -> Command {
     cmd
 }
 
-fn installed_list() -> String {
+fn installed_list() -> Vec<Formula> {
     let output = get_installed_info_command()
         .output()
         .expect("failed to execute process");
     let s = String::from_utf8(output.stdout).expect("no utf8");
-    s
+    serde_json::from_str(&s.into_boxed_str()).expect("hoge")
 }
