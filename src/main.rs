45fe822860388df9217ce2e74304603e61de15af
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-22 17:32:57 +0900

    modified command for get installed formula information

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use std::process::Command;
-use std::str::FromStr;
 use std::string::String;
 
 fn main() {
-    println!("{:?}", installed_list());
+    let c = installed_list();
+    println!("{:?}", c);
 }
 
-fn installed_list() -> Vec<String> {
-    let output = Command::new("brew")
-        .arg("list")
-        .arg("--full-name")
+fn get_installed_info_command() -> Box<Command> {
+    let mut cmd = Command::new("brew");
+    cmd
+        .arg("info")
+        .arg("--json=v1")
+        .arg("--installed");
+    Box::new(cmd)
+}
+
+fn installed_list() -> String {
+    let output = get_installed_info_command()
         .output()
         .expect("failed to execute process");
     let s = String::from_utf8(output.stdout).expect("no utf8");
-    s.split('\n')
-        .filter(|x| *x != "")
-        .map(|s| String::from_str(s).expect(""))
-        .collect()
+    s
 }
